d618f04ff3353a681cf7ab4421b96c77c7162141
Leonardo <leonardo@LEONARDO>
2024-11-12 07:08:29 -0300

    image bg color changed

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -3,7 +3,7 @@ import banner from '@/assets/img/banner.png'
 </script>
 
 <template>
-    <div class="mx-auto max-w-screen-2xl">
+    <div class="mx-auto max-w-screen-2xl bg-blue-950">
         <img class="w-fit max-h-screen aspect-ratio mx-auto flex justify-center" :src="banner" alt="banner" />
     </div>
 </template>
